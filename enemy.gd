cb6ee4e8ffc9266f464300185eb972b13b02a4d7
magic-peach <akankshatrehun@gmail.com>
2024-03-31 23:41:22 +0530

    enemies

--- enemy.gd
+++ enemy.gd
@@ -4,9 +4,18 @@ var velocity = Vector2()
 export var direction = -1 
 
 func _ready():
-	pass
+	if direction == 1:
+		$AnimatedSprite.flip_h = true
+	$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	
 	
 func _physics_process(delta):
+	
+	if is_on_wall() or not $floor_checker.is_colliding() and is_on_floor():
+		direction = direction * -1
+		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+		$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+		
 	velocity.y += 20
 	velocity.x = 50 * direction
-	move_and_slide(velocity)
+	velocity = move_and_slide(velocity,Vector2.UP)
