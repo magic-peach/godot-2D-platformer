0927f1523d91d3b2920f12602d2c0be4b93762c3
magic-peach <akankshatrehun@gmail.com>
2024-04-01 13:57:14 +0530

    enemy changes

--- enemy.gd
+++ enemy.gd
@@ -10,6 +10,8 @@ func _ready():
 		$AnimatedSprite.flip_h = true
 	$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
 	$floor_checker.enabled = detects_cliffs
+	if detects_cliffs:
+		set_modulate(Color(1.2,0,5,1))
 	
 	
 func _physics_process(delta):
@@ -27,3 +29,18 @@ func _physics_process(delta):
 func _on_top_checker_body_entered(body):
 	$AnimatedSprite.play("squashed")
 	speed = 0
+	set_collision_layer_bit(4,false)
+	set_collision_mask_bit(0,false)
+	$top_checker.set_collision_layer_bit(4,false)
+	$top_checker.set_collision_mask_bit(0,false)
+	$sides_checker.set_collision_layer_bit(4,false)
+	$sides_checker.set_collision_mask_bit(0,false)
+	$Timer.start()
+
+
+func _on_sides_checker_body_entered(body):
+	body.ouch(position.x)
+
+
+func _on_Timer_timeout():
+	queue_free()
--- player.gd
+++ player.gd
@@ -32,11 +32,31 @@ func _physics_process(delta):
 	velocity.x = lerp(velocity.x,0,0.4)
 	
 	
-		
-		
-
 
 func _on_fallzone_body_entered(body):
 	get_tree().change_scene("res://Level1.tscn")
 
 
+func bounce():
+	velocity.y = JUMPFORCE * 0.5
+	
+	
+func ouch(var enemyposx):
+	set_modulate(Color(1,0.3,0.3,0.3))
+	velocity.y = JUMPFORCE * 0.3
+	
+	if position.x < enemyposx:
+		velocity.x = -800
+	elif position.x > enemyposx:
+		velocity.x = 800
+		
+		
+	Input.action_release("left")
+	Input.action_release("right")
+	$Timer.start()
+	
+	
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://Level1.tscn")
